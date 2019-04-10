51a9c6f7856fd8d7ba260a3ce92a4b111a8fc048
z0 <ji9310@naver.com>
2019-04-10 10:20:55 +0900

    메인화면 UI

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,44 @@ class MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('Flutter Demo'), // app bar title
+          title: Text('Weather App'), // app bar title
         ),
         body: Center(
-          child: Text('Hello, world'), // center text
+          child: _getTodayWeather(), // center text
         ),
       ),
+    );
+  }
+
+  Widget _getTodayWeather() {
+    return Column (
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Text('용인시', style: TextStyle(color: Colors.white),),
+              Text('맑음', style: TextStyle(color: Colors.white, fontSize: 32.0),),
+              Text('10°C', style: TextStyle(color: Colors.white),),
+              Image.network('https://openweathermap.org/img/w/01d.png'),
+              Text('2019년 4월 10일', style: TextStyle(color: Colors.white),),
+              Text('AM 10:16', style: TextStyle(color: Colors.white),),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: IconButton(
+              icon: Icon(Icons.refresh),
+              tooltip: 'Refresh',
+              onPressed: () => null,
+              color: Colors.white,
+          )
+        )
+      ],
     );
   }
 }
